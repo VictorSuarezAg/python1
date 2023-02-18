6716d236d184ed575f4430eb9f9fb25611533019
Victor Suarez <vasuarezag@cesde.net>
2023-02-18 12:03:25 -0500

    NT2:Se crea archivo gitignore y archivo main.py con variables de 2 numeros y sumatoria

---
+++ main.py
@@ -0,0 +1,7 @@
+numero1 = int(input('Digite un numero: '))
+numero2 = int(input('Digite otro numero: '))
+print(f'El primer numero es {numero1}')
+print(f'El segundo numero es {numero2}')
+
+suma = numero1 + numero2
+print(f'La suma de los dos numeros es {suma}')
